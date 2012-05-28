0e0030a56e3a56f833c0a66e5d054e212f908386
Alex <asotobu@gmail.com>
2012-05-28 19:34:44 +0200

    Add in memory test.

---
+++ nosqlunit-core/src/test/java/com/lordofthejars/nosqlunit/mongodb/integration/WhenInMemoryMongoDbRuleIsRegistered.java
@@ -0,0 +1,43 @@
+package com.lordofthejars.nosqlunit.mongodb.integration;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+import jmockmongo.MockMongo;
+
+import org.junit.Test;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+import com.lordofthejars.nosqlunit.mongodb.InMemoryMongoDb;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import com.mongodb.Mongo;
+
+public class WhenInMemoryMongoDbRuleIsRegistered {
+
+	@Test
+	public void embedded_mongodb_should_be_started_and_stopped() throws Throwable {
+
+		InMemoryMongoDb inMemoryMongoDb = new InMemoryMongoDb();
+
+		Statement noStatement = new Statement() {
+
+			@Override
+			public void evaluate() throws Throwable {
+				Mongo mongo = new Mongo("0.0.0.0", MockMongo.DEFAULT_PORT);
+				mongo.getDB("test").getCollection("test").insert(new BasicDBObject("name", "Alex"));
+				mongo.close();
+				
+				Mongo mongoQuery = new Mongo("0.0.0.0", MockMongo.DEFAULT_PORT);
+				DBObject nameDbObject = mongoQuery.getDB("test").getCollection("test").findOne(new BasicDBObject("name", "Alex"));
+				assertThat((String)nameDbObject.get("name"), is("Alex"));
+				
+			}
+		};
+		
+		Statement decotedStatement = inMemoryMongoDb.apply(noStatement, Description.EMPTY);
+		decotedStatement.evaluate();
+
+	}
+
+}
