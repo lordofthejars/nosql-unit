b84ee88cb55b423999f437b652eff998bb102952
Alex Soto <asotobu@gmail.com>
2013-02-26 21:23:56 +0100

    Fixes #56 by retrieving firstinstance of GDS.

--- nosqlunit-neo4j/src/main/java/com/lordofthejars/nosqlunit/neo4j/SpringGraphDatabaseServiceNeo4jRule.java
+++ nosqlunit-neo4j/src/main/java/com/lordofthejars/nosqlunit/neo4j/SpringGraphDatabaseServiceNeo4jRule.java
@@ -1,5 +1,13 @@
 package com.lordofthejars.nosqlunit.neo4j;
 
+import static ch.lambdaj.Lambda.selectFirst;
+import static ch.lambdaj.Lambda.having;
+import static ch.lambdaj.Lambda.on;
+import static ch.lambdaj.collection.LambdaCollections.with;
+import static org.hamcrest.CoreMatchers.anything;
+
+import java.util.Map;
+
 import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.Statement;
 import org.neo4j.graphdb.GraphDatabaseService;
@@ -10,7 +18,7 @@ import com.lordofthejars.nosqlunit.core.PropertyGetter;
 class SpringGraphDatabaseServiceNeo4jRule extends Neo4jRule {
 
 	private PropertyGetter<ApplicationContext> propertyGetter = new PropertyGetter<ApplicationContext>();
-	
+
 	public SpringGraphDatabaseServiceNeo4jRule(Neo4jConfiguration neo4jConfiguration) {
 		super(neo4jConfiguration);
 	}
@@ -24,10 +32,20 @@ class SpringGraphDatabaseServiceNeo4jRule extends Neo4jRule {
 		this.databaseOperation = new Neo4jOperation(definedGraphDatabaseService(testObject));
 		return super.apply(base, method, testObject);
 	}
-	
+
 	private GraphDatabaseService definedGraphDatabaseService(Object testObject) {
 		ApplicationContext applicationContext = propertyGetter.propertyByType(testObject, ApplicationContext.class);
-		return applicationContext.getBean(GraphDatabaseService.class);
+
+		Map<String, GraphDatabaseService> beansOfType = applicationContext.getBeansOfType(GraphDatabaseService.class);
+		GraphDatabaseService graphDatabaseService = with(beansOfType).values().first(anything());
+
+		if (graphDatabaseService == null) {
+			throw new IllegalArgumentException(
+					"At least one GraphDatabaseService instance should be defined into Spring Application Context.");
+		}
+
+		return graphDatabaseService;
+
 	}
-	
+
 }
