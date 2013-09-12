c9ea87dc4e9a209d99aba6fa5481daf56ad0e13d
Stewart <stewart@javahelp.co.uk>
2013-09-12 11:30:01 +0100

    Updated Neo4j TypeCaster handling of arrays

--- nosqlunit-neo4j/src/main/java/com/lordofthejars/nosqlunit/graph/parser/TypeCaster.java
+++ nosqlunit-neo4j/src/main/java/com/lordofthejars/nosqlunit/graph/parser/TypeCaster.java
@@ -2,13 +2,33 @@ package com.lordofthejars.nosqlunit.graph.parser;
 
 import java.lang.reflect.Array;
 import java.lang.reflect.Constructor;
+import java.util.HashMap;
 import java.util.Map;
-
-import org.apache.commons.lang.StringUtils;
 
 import com.google.common.primitives.Primitives;
 
-public class TypeCaster {
+public final class TypeCaster {
+
+	private static final Map<String, String>	types	= new HashMap<String, String>();
+
+	static {
+		try {
+			putNumberType(Boolean.class);
+			putNumberType(Character.class);
+			putNumberType(Byte.class);
+			putNumberType(Short.class);
+			putNumberType(Integer.class);
+			putNumberType(Long.class);
+			putNumberType(Float.class);
+			putNumberType(Double.class);
+		} catch (final Exception e) {
+			throw new IllegalStateException(e);
+		}
+	}
+
+	private static void putNumberType(final Class<?> primitiveClass) throws IllegalArgumentException, SecurityException, IllegalAccessException, NoSuchFieldException {
+		types.put(primitiveClass.getField("TYPE").get(null).toString(), primitiveClass.getName());
+	}
 
 	private TypeCaster() {
 		super();
@@ -28,7 +48,8 @@ public class TypeCaster {
 		} else if (object instanceof Boolean) {
 			return GraphMLTokens.BOOLEAN;
 		} else if (isArray(object)) {
-		    return object.getClass().getSimpleName();
+			final Object first = Array.get(object, 0);
+			return getStringType(first) + "[]";
 		} else {
 			return GraphMLTokens.STRING;
 		}
@@ -37,45 +58,46 @@ public class TypeCaster {
 	public static boolean isArray(Object obj) {
 	    return obj!=null && obj.getClass().isArray();
 	}
-	
+
 	public static Object typeCastValue(String key, String value, Map<String, String> keyTypes) {
 		String type = keyTypes.get(key);
-		if (null == type || type.equals(GraphMLTokens.STRING))
+		if (null == type || type.equals(GraphMLTokens.STRING)) {
 			return value;
-		else if (type.equals(GraphMLTokens.FLOAT))
+		} else if (type.equals(GraphMLTokens.FLOAT)) {
 			return Float.valueOf(value);
-		else if (type.equals(GraphMLTokens.INT))
+		} else if (type.equals(GraphMLTokens.INT)) {
 			return Integer.valueOf(value);
-		else if (type.equals(GraphMLTokens.DOUBLE))
+		} else if (type.equals(GraphMLTokens.DOUBLE)) {
 			return Double.valueOf(value);
-		else if (type.equals(GraphMLTokens.BOOLEAN))
+		} else if (type.equals(GraphMLTokens.BOOLEAN)) {
 			return Boolean.valueOf(value);
-		else if (type.equals(GraphMLTokens.LONG))
+		} else if (type.equals(GraphMLTokens.LONG)) {
 			return Long.valueOf(value);
-		else if (type.contains("[]"))
-            return castToArray(type, value);
-		else
+		} else if (type.contains("[]")) {
+			return castToArray(type, value);
+		} else {
 			return value;
+		}
 	}
 
 	/**
-     * Creates a primitive array of the specified type (eg. long[] or int[])
-     * Using 'value' as a comma-delimited String.
-     */
-    static Object castToArray(final String type, final String value) {
-        try {
-            final String[] values = value.split(",");
-            final String className = "java.lang." + StringUtils.capitalize(type.replace("[]", ""));
-            final Class<?> klass = Class.forName(className);
-            final Constructor<?> konstructor = klass.getDeclaredConstructor(String.class);
-            final Object array = Array.newInstance(Primitives.unwrap(klass), values.length);
-            for (int i = 0; i < values.length; i++) {
-                Array.set(array, i, konstructor.newInstance(values[i].trim()));
-            }
-            return array;
-        } catch (final Exception e) {
-            throw new IllegalStateException("Could not cast " + value + " to " + type, e);
-        }
-    }
-	
+	 * Creates a primitive array of the specified type (eg. long[] or int[])
+	 * Using 'value' as a comma-delimited String.
+	 */
+	static Object castToArray(final String type, final String value) {
+		try {
+			final String[] values = value.split(",");
+			final String className = types.get(type.replace("[]", ""));
+			final Class<?> klass = Class.forName(className);
+			final Constructor<?> konstructor = klass.getDeclaredConstructor(String.class);
+			final Object array = Array.newInstance(Primitives.unwrap(klass), values.length);
+			for (int i = 0; i < values.length; i++) {
+				Array.set(array, i, konstructor.newInstance(values[i].trim()));
+			}
+			return array;
+		} catch (final Exception e) {
+			throw new IllegalStateException("Could not cast " + value + " to " + type, e);
+		}
+	}
+
 }
