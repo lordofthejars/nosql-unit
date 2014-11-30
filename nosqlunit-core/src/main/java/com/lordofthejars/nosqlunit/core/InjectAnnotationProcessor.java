39d4879d5c094363a78e82e127bdc586f9ee46c7
Alex Soto <asotobu@gmail.com>
2014-11-30 13:00:35 +0100

    resolves issue #115

--- nosqlunit-core/src/main/java/com/lordofthejars/nosqlunit/core/InjectAnnotationProcessor.java
+++ nosqlunit-core/src/main/java/com/lordofthejars/nosqlunit/core/InjectAnnotationProcessor.java
@@ -1,7 +1,13 @@
 package com.lordofthejars.nosqlunit.core;
 
+import java.security.PrivilegedAction;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
+import java.security.AccessController;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -11,17 +17,22 @@ import com.lordofthejars.nosqlunit.annotation.ConnectionManager;
 
 public class InjectAnnotationProcessor {
 
-	private String identifier;
+    private String identifier;
 
-	public InjectAnnotationProcessor(String identifier) {
-		this.identifier = identifier;
-	}
+    public InjectAnnotationProcessor(String identifier) {
+        this.identifier = identifier;
+    }
 
-	void processInjectAnnotation(Class<?> clazz, Object testInstance,
+    void processInjectAnnotation(Class<?> clazz, Object testInstance,
 			Object injectionObject) {
 
 		if (isTargetSet(testInstance)) {
-			Field[] fields = clazz.getDeclaredFields();
+		    
+		    Set<Field> fields = new HashSet<Field>();
+		    fields.addAll(getFieldsWithAnnotation(clazz, Inject.class));
+		    fields.addAll(getFieldsWithAnnotation(clazz, ConnectionManager.class));
+		    fields.addAll(getFieldsWithAnnotation(clazz, ByContainer.class));
+		    
 			for (Field field : fields) {
 				Annotation injectAnnotation = field.getAnnotation(Inject.class);
 				Annotation connectionManagerAnnotation = field.getAnnotation(ConnectionManager.class);
@@ -44,22 +55,48 @@ public class InjectAnnotationProcessor {
 		}
 	}
 
-	private boolean isInjectedAndNotExternallyManaged(Annotation injectAnnotation, Annotation byContainerAnnotation, Annotation connectionManagerAnnotation) {
-		return (injectAnnotation != null && byContainerAnnotation == null) || connectionManagerAnnotation != null;
-	}
+    public static List<Field> getFieldsWithAnnotation(final Class<?> source,
+            final Class<? extends Annotation> annotationClass) {
+        List<Field> declaredAccessableFields = AccessController
+                .doPrivileged(new PrivilegedAction<List<Field>>() {
+                    public List<Field> run() {
+                        List<Field> foundFields = new ArrayList<Field>();
+                        Class<?> nextSource = source;
+                        while (nextSource != Object.class) {
+                            for (Field field : nextSource.getDeclaredFields()) {
+                                if (field.isAnnotationPresent(annotationClass)) {
+                                    if (!field.isAccessible()) {
+                                        field.setAccessible(true);
+                                    }
+                                    foundFields.add(field);
+                                }
+                            }
+                            nextSource = nextSource.getSuperclass();
+                        }
+                        return foundFields;
+                    }
+                });
+        return declaredAccessableFields;
+    }
 
-	private boolean isIdentifierValueInNamedAnnotation(
-			Annotation namedAnnotation) {
-		String namedValue = ((Named) namedAnnotation).value();
-		return "".equals(namedValue) || namedValue.equals(
-				identifier);
-	}
+    private boolean isInjectedAndNotExternallyManaged(
+            Annotation injectAnnotation, Annotation byContainerAnnotation,
+            Annotation connectionManagerAnnotation) {
+        return (injectAnnotation != null && byContainerAnnotation == null)
+                || connectionManagerAnnotation != null;
+    }
 
-	private boolean isNamedAnnotationNotPresent(Annotation namedAnnotation) {
-		return namedAnnotation == null;
-	}
+    private boolean isIdentifierValueInNamedAnnotation(
+            Annotation namedAnnotation) {
+        String namedValue = ((Named) namedAnnotation).value();
+        return "".equals(namedValue) || namedValue.equals(identifier);
+    }
 
-	private boolean isTargetSet(Object testInstance) {
-		return testInstance != null;
-	}
+    private boolean isNamedAnnotationNotPresent(Annotation namedAnnotation) {
+        return namedAnnotation == null;
+    }
+
+    private boolean isTargetSet(Object testInstance) {
+        return testInstance != null;
+    }
 }
