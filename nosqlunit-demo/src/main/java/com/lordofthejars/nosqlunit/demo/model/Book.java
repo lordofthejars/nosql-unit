e14454d3d2c6e719f69b8d7417789f63d4a5cf46
Alex Soto <asotobu@gmail.com>
2014-12-01 09:42:24 +0100

    Implements Eauls and HashCode in Book class.

--- nosqlunit-demo/src/main/java/com/lordofthejars/nosqlunit/demo/model/Book.java
+++ nosqlunit-demo/src/main/java/com/lordofthejars/nosqlunit/demo/model/Book.java
@@ -26,5 +26,33 @@ public class Book {
     public void setNumberOfPages(int numberOfPages) {
         this.numberOfPages = numberOfPages;
     }
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + numberOfPages;
+        result = prime * result + ((title == null) ? 0 : title.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Book other = (Book) obj;
+        if (numberOfPages != other.numberOfPages)
+            return false;
+        if (title == null) {
+            if (other.title != null)
+                return false;
+        } else if (!title.equals(other.title))
+            return false;
+        return true;
+    }
 	
 }
